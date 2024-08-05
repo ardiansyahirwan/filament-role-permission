24a2905784aa90d1ece8396aaa58530a966fa322
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2024-08-06 04:03:30 +0700

    rename namespace to Ardiansyahirwan\FilamentRolePermission\Filename

--- src/CustomPermissionServiceProvider.php
+++ src/CustomPermissionServiceProvider.php
@@ -59,10 +59,10 @@ class CustomPermissionServiceProvider extends ServiceProvider
    protected function loadSeeder()
    {
       Artisan::call('db:seed', [
-         '--class' => "Ardiansyahirwan\\FilamentRolePermission\\Seeders\\CustomRoleSeeder",
+         '--class' => "Ardiansyahirwan\\FilamentRolePermission\\CustomRoleSeeder",
       ]);
       Artisan::call('db:seed', [
-         '--class' => "Ardiansyahirwan\\FilamentRolePermission\\Seeders\\CustomPermissionSeeder",
+         '--class' => "Ardiansyahirwan\\FilamentRolePermission\\CustomPermissionSeeder",
       ]);
    }
 }
--- src/database/seeders/CustomPermissionSeeder.php
+++ src/database/seeders/CustomPermissionSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ardiansyahirwan\FilamentRolePermission\Seeders;
+namespace Ardiansyahirwan\FilamentRolePermission;
 
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
