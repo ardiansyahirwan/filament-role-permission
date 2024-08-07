6bf16faa4d0f6e8ef0c19c554b4c06184de7651c
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2024-08-07 13:02:43 +0700

    Add console file

---
+++ src/Console/InstallRolePermissionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ardiansyahirwan\FilamentRolePermission\Console;
+
+use Ardiansyahirwan\FilamentRolePermission\Database\Seeders\CustomPermissionSeeder;
+use Ardiansyahirwan\FilamentRolePermission\Database\Seeders\CustomRoleSeeder;
+use Illuminate\Console\Command;
+
+class InstallRolePermissionSeeder extends Command
+{
+   /**
+    * The name and signature of the console command.
+    *
+    * @var string
+    */
+   protected $signature = 'ardiansyah:seed';
+
+   /**
+    * The console command description.
+    *
+    * @var string
+    */
+   protected $description = 'Make a seeder for role and permission table';
+
+   /**
+    * Execute the console command.
+    */
+   public function handle()
+   {
+      $this->call(CustomRoleSeeder::class);
+      $this->call(CustomPermissionSeeder::class);
+   }
+}
--- src/CustomPermissionServiceProvider.php
+++ src/CustomPermissionServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Ardiansyahirwan\FilamentRolePermission;
 
+use Ardiansyahirwan\FilamentRolePermission\Console\InstallRolePermissionSeeder;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
 use Filament\Facades\Filament;
@@ -21,49 +22,15 @@ class CustomPermissionServiceProvider extends ServiceProvider
 
    public function boot()
    {
-      // Ensure the PermissionServiceProvider is registered.
-      $this->app->register(PermissionServiceProvider::class);
       // Publish configuration file
       $this->publishes([
          __DIR__ . '/../config/filament-custom-role-permission.php' => config_path('filament-custom-role-permission.php'),
       ], 'config');
 
-      // call seeder with artisan db:seed
-      $this->callSeeder();
-
-      // Example: Add custom Filament resources
-      Filament::serving(function () {
-         Filament::registerNavigationItems([
-            NavigationItem::make('Roles & Permissions')
-               ->url(route('filament.resources.roles.index'))
-               ->icon('heroicon-o-shield-check'),
-         ]);
-      });
-   }
-
-   protected function callSeeder()
-   {
       if ($this->app->runningInConsole()) {
-         $this->publishPermission();
-         // $this->loadSeeder();
+         $this->commands([
+            InstallRolePermissionSeeder::class
+         ]);
       }
-   }
-
-   protected function publishPermission()
-   {
-      Artisan::call('vendor:publish', [
-         '--provider' => "Spatie\\Permission\\PermissionServiceProvider",
-      ]);
-   }
-
-   protected function loadSeeder()
-   {
-      Artisan::call('db:seed', [
-         '--class' => "Ardiansyahirwan\\FilamentRolePermission\\Database\\Seeders\\CustomRoleSeeder",
-      ]);
-
-      Artisan::call('db:seed', [
-         '--class' => "Ardiansyahirwan\\FilamentRolePermission\\Database\\Seeders\\CustomPermissionSeeder",
-      ]);
    }
 }
