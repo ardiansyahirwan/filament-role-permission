6862e4c4842b46c9b4a1faf5eaf754a53fe9ee4d
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2024-08-19 16:11:41 +0700

    chore: add policies file and config at service provider

--- src/CustomPermissionServiceProvider.php
+++ src/CustomPermissionServiceProvider.php
@@ -2,17 +2,26 @@
 
 namespace Ardiansyahirwan\FilamentRolePermission;
 
+use App\Policies\PermissionPolicy;
+use App\Policies\RolePolicy;
 use Ardiansyahirwan\FilamentRolePermission\Console\InstallRolePermissionSeeder;
-use Illuminate\Support\ServiceProvider;
-use Spatie\Permission\PermissionServiceProvider;
-use Filament\Facades\Filament;
-use Filament\Navigation\NavigationItem;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+// use Illuminate\Support\ServiceProvider;
 
 class CustomPermissionServiceProvider extends ServiceProvider
 {
+   // Policies File
+   protected $policies = [
+      Role::class => RolePolicy::class,
+      Permission::class => PermissionPolicy::class,
+   ];
+
    public function register()
    {
+
       // Register any application services.
       $this->mergeConfigFrom(
          __DIR__ . '/../config/custompermission.php',
@@ -22,6 +31,11 @@ class CustomPermissionServiceProvider extends ServiceProvider
 
    public function boot()
    {
+      // Publish Policies File
+      $this->registerPolicies();
+      $this->publishes([
+         __DIR__ . '/../src/Policies' => app_path('Policies'),
+      ], 'policies');
       // Publish configuration file
       $this->publishes([
          __DIR__ . '/../config/custompermission.php' => config_path('custompermission.php'),
